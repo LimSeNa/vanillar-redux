83e522d23dd36cac5efba46402605ad41cfc4eec
Sena Lim <tpsk8322@naver.com>
2023-05-22 15:35:50 +0900

    16장. 리덕스 라이브러리 이해하기

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { legacy_createStore as createStore } from 'redux';
+import {createStore} from 'redux';
 
 const divToggle = document.querySelector('.toggle');
 const counter = document.querySelector('h1');
